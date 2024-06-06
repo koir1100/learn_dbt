e0f087500ac216dd21868d4ee2e96240327752cd
Yonggu Choi <koir1100@gmail.com>
2024-06-06 19:44:32 +0900

    feat. input-output model contents apply
    - staging query: src directory
    - core query(first): fact, dim directory

---
+++ models/src/src_user_event.sql
@@ -0,0 +1,13 @@
+WITH src_user_event AS
+(
+    SELECT * FROM raw_data.user_event
+)
+SELECT
+    user_id,
+    datestamp,
+    item_id,
+    clicked,
+    purchased,
+    paidamount
+FROM
+    src_user_event
